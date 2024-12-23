7b00b5cada2b21793c05bd731a9026aaec2b43be
Nail Abduzhalenov <rolla1n1rock@gmail.com>
2024-12-23 16:43:35 +0500

    add test

---
+++ bot_dz.py
@@ -0,0 +1,48 @@
+import asyncio
+import logging
+import random
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
+from aiogram.types import Message
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token='7325923176:AAFz7tbO6WLejOfDL73k5Jw6jkfThATpQO8')
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def cmd_name(message: Message):
+    await message.reply("День добрый, чтобы узнать доступные команды этого бота - используйте /help")
+
+@dp.message(Command("help"))
+async def cmd_name(message: Message):
+    await message.reply("Доступные команды: \n/start \n/help \n/name \n/joke \n/stop")
+
+@dp.message(Command("name"))
+async def cmd_name(message: Message):
+    args = message.text.split(maxsplit=1)
+    if len(args) > 1:
+        await message.answer(f"Hi, <b>{args[1]}</b>", parse_mode="HTML")
+    else:
+        await message.answer("Чтобы бот знал ваше имя, напишите его после команды /name")
+
+jokes = ["Что общего у любви и Wi-Fi? На расстоянии начинаются проблемы",
+         "Мам, смотри, голубь! У тебя хлеб есть? Без хлеба ешь!",
+         "Из студенческого общежития куда-то исчезли все кошки… Вот такие пироги!",
+         "Аня вышла замуж за механика и родила шестерню",
+         "Обожаю гулять по болотам! Очень затягивает"
+]
+
+@dp.message(Command("joke"))
+async def send_joke(message: Message):
+    joke = random.choice(jokes)
+    await message.answer(joke)
+
+@dp.message(Command("stop"))
+async def cmd_name(message: Message):
+    await message.reply("До свидания")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ test.py
@@ -0,0 +1,37 @@
+#                                                   Задание 1
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i)
+
+
+#                                                   Задание 2
+# n = int(input("enter n: "))
+# sum = 0
+# for i in range(1, n):
+#     sum = sum + i
+# print(sum)
+
+
+#                                                   Задание 3
+# n = int(input("Enter your number: "))
+
+# print(f"Таблица умножения для числа {n}: ")
+# for i in range(1, 11):
+#     print(f"{n} * {i} = {n * i}")
+
+
+#                                                   Задание 4
+# for i in range(50, 101):
+#     if i % 2 != 0:
+#         print(i)
+
+
+# vowels = "аеёиоуыэюя"
+
+# n = input("Введите свое слово: ")
+# count = 0
+# for i in n.lower():
+#     if i in vowels:
+#         count += 1
+
+# print(f"гласных букв в вашем слове: {count}")
